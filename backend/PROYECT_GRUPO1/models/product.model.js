2c7a20ea075cee14930fe80c993495864eb97792
JulianGiraldoR <juliangato87@gmail.com>
2022-09-29 21:25:55 -0500

    backend proyect v1.0

---
+++ backend/PROYECT_GRUPO1/models/product.model.js
@@ -0,0 +1,30 @@
+//modelo products
+
+const mongoose = require("mongoose");
+
+const productSchema =  new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,  
+  },
+  description: {
+    type: String,
+    required: true,  
+  },
+  category: {
+    type: String,
+    required: true,
+    
+  },
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String ,
+    required: false,
+  }
+ 
+},{timestamps: true})
+
+module.exports = mongoose.model("Product", productSchema);
